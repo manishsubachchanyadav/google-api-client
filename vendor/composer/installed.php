374d9560330e4451b0fffdb4cd4f9aee4d22d5ce
Manish Yadav <msubachchanyadav@vendasta.com>
2024-05-25 11:19:37 +0530

    Add file in Composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '50c9fe7c325a75bfcb65800a006732b7a89e4890',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '50c9fe7c325a75bfcb65800a006732b7a89e4890',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
